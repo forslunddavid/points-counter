371c03f28338110a5ccad8d28ff1e06593536c41
Forslunddavid <forslund.david@gmail.com>
2024-07-07 22:11:12 +0200

    fixed gh-pages branch

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
 export default defineConfig({
-	base: "/points-counter/", // Ensure this matches your repository name
+	base: "/points-counter/",
 	plugins: [react()],
 })
